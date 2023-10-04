ba64693940c9e54517afa8f858e97d960b183c7d
Snezhaly <adventchiken@gmail.com>
2023-10-05 02:40:50 +0500

    Начинаем программировать

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+
+const mainProps = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  dateFilm: "2014"
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +14,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App name={mainProps.name} genre={mainProps.genre} dateFilm={mainProps.dateFilm} />
   </React.StrictMode>
 );
