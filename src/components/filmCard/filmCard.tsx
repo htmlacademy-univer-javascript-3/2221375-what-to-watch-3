15225e8b0b2d285709ca1ebca5e604ff0f6f753e
Snezhaly <adventchiken@gmail.com>
2023-11-19 17:01:31 +0500

    больше подробностей (часть 1)

--- src/components/filmCard/filmCard.tsx
+++ src/components/filmCard/filmCard.tsx
@@ -1,20 +1,27 @@
 import { Link } from 'react-router-dom';
+import { videoSrc } from '../../mocks/video';
+import VideoPlayer from '../videoPlayer/videoPlayer';
 
 type FilmCardProps = {
   name: string;
   previewImage: string;
   id: string;
   changeSelectedFilm: (id: string) => void;
+  isSelected: boolean;
 }
 
 function FilmCard(props: FilmCardProps): JSX.Element {
   return (
     <article onMouseOver={() => {
       props.changeSelectedFilm(props.id);
-    }} className="small-film-card catalog__films-card"
+    }}
+    onMouseLeave={() => {
+      props.changeSelectedFilm('');
+    }}
+    className="small-film-card catalog__films-card"
     >
       <div className="small-film-card__image">
-        <img src={props.previewImage} alt={props.name} width="280" height="175" />
+        <VideoPlayer videoSrc={videoSrc} isActive={props.isSelected} poster={props.previewImage} />
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${props.id}`}>{props.name}</Link>
